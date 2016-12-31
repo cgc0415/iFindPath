f53ded4d2f56269ce360cc6059ad130ae7b95001
ross <867448685@qq.com>
2016-12-31 19:17:31 +0800

    实现方块自动移动.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,11 @@
 
 using namespace std;
 
-GLfloat rtx = 0.0f, rty = 0.0f, rtz = 0.0f;
+#define TIMER_INTERVAL_VALUE 800
+#define WINDOW_SIZE_X 400
+#define WINDOW_SIZE_Y 400
+
+GLfloat rtx = 0.8f, rty = 0.0f, rtz = 0.0f;
 GLfloat step = 0.05;
 GLfloat exp = 1e-3;
 
@@ -22,19 +26,28 @@ void draw(void)
 	printf("%f %f %f\n", rtx, rty, rtz);
 	glTranslatef(rtx, rty, rtz);
 
-	glBegin(GL_QUADS);
-		glColor3f(1.0f, 0.0f, 0.0f);
+	/*glBegin(GL_LINE_STRIP);	    
 		glVertex2f(-0.5f, 0.5f);
-		glColor3f(0.0f, 1.0f, 0.0f);
-		glVertex2f(0.5f, 0.5f);
-		glColor3f(0.0f, 0.0f, 1.0f);
-		glVertex2f(0.5f, -0.5f);
-		glColor3f(0.5f, 0.5f, 0.5f);
 		glVertex2f(-0.5f, -0.5f);
-	glEnd();
+		glVertex2f(0.5f, -0.5f);
+		glVertex2f(0.5f, 0.5f);
+		glVertex2f(-0.5f, 0.5f);
+	glEnd();*/
+
+	glRectf(-0.25,0.25,0.25,-0.25);
 
 	glPopMatrix();
 	glutSwapBuffers();
+}
+
+void timerFunction(int value)
+{
+	if(rtx + 0.7 <= 0.001)
+		rtx = 0;
+	rtx -= 0.1;
+	
+	glutPostRedisplay();
+	glutTimerFunc(TIMER_INTERVAL_VALUE, timerFunction, 1);
 }
 
 void processKeyBoard(int key, int x, int y)
@@ -66,7 +79,7 @@ int main(int argc, char *argv[])
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
 	glutInitWindowPosition(100, 100);
-	glutInitWindowSize(200, 200);
+	glutInitWindowSize(WINDOW_SIZE_X, WINDOW_SIZE_Y);
 	glutCreateWindow("test");
 
 	init();
@@ -74,6 +87,9 @@ int main(int argc, char *argv[])
 	glutDisplayFunc(draw);
 	//glutIdleFunc(move);
 	glutSpecialFunc(processKeyBoard);
+
+	glutTimerFunc(TIMER_INTERVAL_VALUE, timerFunction, 1);
+	glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
 	glutMainLoop();
 	return 0;
 }
